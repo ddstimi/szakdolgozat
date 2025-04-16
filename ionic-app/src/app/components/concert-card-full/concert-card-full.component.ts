f67d5500f0aac5c43bf3eb701a2df8343dab4cb6
Dudas Timea <h257330@stud.u-szeged.hu>
2025-04-16 13:24:28 +0200

    fix(concert-card-full): fixed design and added them to home page

--- ionic-app/src/app/components/concert-card-full/concert-card-full.component.ts
+++ ionic-app/src/app/components/concert-card-full/concert-card-full.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import {IonCardContent, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonImg } from "@ionic/angular/standalone";
+import {IonCardContent, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonImg, IonIcon, IonChip } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-concert-card-full',
   templateUrl: './concert-card-full.component.html',
   styleUrls: ['./concert-card-full.component.scss'],
   standalone:true,
-    imports: [IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonImg, IonCard, CommonModule], // Required for structural directives like *ngIf, *ngFor
+    imports: [IonChip, IonIcon, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonImg, IonCard, CommonModule], // Required for structural directives like *ngIf, *ngFor
   
 
 })
